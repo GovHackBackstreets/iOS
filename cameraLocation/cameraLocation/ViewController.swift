6c96e73d18b136291a813656e802161f7eb17687
Landon Vago-Hughes <landon2008@hotmail.com>
2017-07-15 15:01:43 +0100

    setup

---
+++ cameraLocation/cameraLocation/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  cameraLocation
+//
+//  Created by Landon Vago-Hughes on 15/07/2017.
+//  Copyright © 2017 Landon Vago-Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+        
+        
+    }
+
+
+}
+
